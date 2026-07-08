5ab3bd9d0ac977605998b2ab9a55b32dbe290dcf
devchuks <emmaantisimp@gmail.com>
2026-07-08 13:42:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let gitEl = document.getElementById("git-el");
+let frontEl = document.getElementById("front-el");
+let linkEl = document.getElementById("link-el");
+let twitterEl = document.getElementById("twitter-el");
+let instaEl = document.getElementById("insta-el");
+
+function github() {
+  gitEl.textContent = "visit @ jessica.github";
+}
+
+function frontend() {
+  frontEl.textContent = "visit @ jessica.frontend";
+}
+
+function linkedin() {
+  linkEl.textContent = "visit @ jessica.linkedin";
+}
+
+function twitter() {
+  twitterEl.textContent = "visit @ jessica.twitter";
+}
+
+function instagram() {
+  instaEl.textContent = "visit @ jessica.instagram";
+}
